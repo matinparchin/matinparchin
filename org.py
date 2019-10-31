d74b158668ae6b3eb46efb4bfa309079c43ec4f9
matinparchin <57220414+matinparchin@users.noreply.github.com>
2019-10-31 15:04:31 +0300

    Update org.py

--- org.py
+++ org.py
@@ -1,16 +1,16 @@
 import builtwith
-wq='\t*******************************************************\n\n\n \t         telegram chanell : @persian_cyber_org \n\n\n\t*******************************************************'
+wq='\t*******************************************************\n\n\n \t         telegram chanell : @matinparchin \n\n\n\t*******************************************************'
 print(wq)
 id=input("\n\nid ? ? ?")
 message=input("message ? ? ?")
 m=message.replace(" ","+")
-pass1='9323'
+pass1='2211'
 q=4
 w=0
 n=input("what is password? ? ?")
 if n == pass1:
     
-    site = "http://persian-cyber.xyz/send-message/AR-MAN.php?message="+m+"&id="+id+"&pass=@Persian_Cyer_Org"
+    site = "http://persian-cyber.xyz/send-message/AR-MAN.php?message="+m+"&id="+id+"&pass=@persian_cyber_org"
     mmd=builtwith.parse(site)
     print("OK ! !  done successfully")
 else :
